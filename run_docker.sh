d1e5c262bee0a397c819ba0f7d5eb9b16ac79c3d
Jyotish Gogoi <jgogoi@wgu.edu>
2020-03-21 10:25:24 +0000

    changes from old VM

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,15 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=makeprediction .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -d -p 8000:80 makeprediction
+
+# debug
+docker run -it --entrypoint /bin/bash makeprediction
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=jygogo/project-ml-microservice-kubernetes
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=jygogo
+docker tag makeprediction $dockerpath:make_prediction_v2
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:make_prediction_v2
